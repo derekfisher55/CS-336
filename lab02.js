01f46ad58fa2c521343026d439b798b07ec5b490
derekfisher55 <43037796+derekfisher55@users.noreply.github.com>
2018-09-14 09:16:11 -0400

    Add files via upload

--- lab02.js
+++ lab02.js
@@ -16,9 +16,9 @@ Person.prototype.changeName = function(newName) {
 
 // Function computes the age of a person object. 
 // Code from: http://jsfiddle.net/codeandcloud/n33RJ/
-Person.prototype.getAge = function(birthDate) {
+Person.prototype.getAge = function() {
 	var today = new Date();
-    var birth = new Date(birthDate);
+    var birth = new Date(this.birthDate);
     var age = today.getFullYear() - birth.getFullYear();
     var m = today.getMonth() - birth.getMonth();
     if (m < 0 || (m === 0 && today.getDate() < birth.getDate())) {
@@ -33,10 +33,28 @@ Person.prototype.addFriend = function(friend) {
 }
 
 // Function gives the person object the ability to print a greeting.
-//Person.prototype.printGreeting = function(greeting) {
-//	return 
-//}
+Person.prototype.printGreeting = function(greeting) {
+	return greeting;
+}
 
+
+// Creates a student class that inherents information from peron class.
+function Student(name, birthDate, friendsList, major) {
+	Person.call(this, name, birthDate, friendsList);
+	this.major = major;
+}
+
+Student.prototype = Object.create(Person.prototype);
+
+// Function gives the student object the ability to print a greeting. 
+Student.prototype.printGreeting = function(greeting) {
+	return greeting;
+}
+
+
+
+
+// Tests: //
 
 var p1 = new Person("Bob", "1990/11/11", ["Billy"]);
 
@@ -46,7 +64,20 @@ p1.changeName("Matt");
 
 console.log(p1);
 
+console.log(p1.printGreeting("hi"));
 
-console.log(p1.getAge());
+
+var s1 = new Student("Billy", "1990/11/11", ["Bob"],"Math");
+
+s1.addFriend("Kim");
+
+console.log(s1);
+
+console.log(s1.printGreeting("hello"));
+
+console.log(s1.getAge());
+
+
+
 
 
