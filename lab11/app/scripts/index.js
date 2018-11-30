7ae90c2bde748646dc424173e85ff43f75d22b98
Derek Fisher <derekfisher@Dereks-MacBook-Pro-5.local>
2018-11-30 09:13:00 -0500

    adding lab11

---
+++ lab11/app/scripts/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentBox from './CommentBox.js';
+import '../css/base.css';
+
+ReactDOM.render(
+  <CommentBox url="/api/comments" pollInterval={2000} />,
+  document.getElementById('content')
+);
